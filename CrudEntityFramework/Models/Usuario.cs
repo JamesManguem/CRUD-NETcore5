902c4b08d052d369a0dcd2db38aa15a1a642e9a6
Jaime <18090067@alumno.utmetropolitana.edu.mx>
2021-04-25 23:31:02 -0500

    CRUD FINALIZADO

---
+++ CrudEntityFramework/Models/Usuario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudEntityFramework.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="El nombre es Obligatorio")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El teléfono es Obligatorio")]
+        [Display(Name ="Teléfono")]
+        public string Telefono { get; set; }
+
+
+        [Required(ErrorMessage = "El Celular es Obligatorio")]
+        public string Celular { get; set; }
+
+
+        [Required(ErrorMessage = "El e-mail es Obligatorio")]
+        public string Email { get; set; }
+
+
+       
+    }
+}
